2ba58881b53328bf1c0944e11f040e7f32afbc8d
Ronald Allan Cruz Patawaran <patawro@mfcgd.com>
2024-03-14 18:50:02 +0800

    Updated code

--- src/pages/Restaurants.jsx
+++ src/pages/Restaurants.jsx
@@ -1,18 +1,15 @@
 import { useState, useEffect } from 'react';
 import { H1, P, Stack } from '@manulife/mux';
 import axios from  "axios";
-import { useSearchParams } from 'react-router-dom';
 
 import RestaurantItem from 'src/components/restaurant/RestaurantItem';
 
 const Restaurant = () => {
   const [restaurants, setRestaurants] = useState([]);
-  const [searchParams] = useSearchParams();
   useEffect(() => {
     axios.get('http://localhost:3001/restaurants').then(response => {
       setRestaurants([...response.data]);
     });
-    console.log(searchParams.get('restaurants'), "searchParams");
   }, []);
 
   return (
